dd3f49bf84a2f53f02d6e78dd8783586a6bcb6c9
Mugundhan <58930635+Mugundhanp2497@users.noreply.github.com>
2019-12-19 17:34:29 +0530

    Add files via upload

---
+++ pro3/src/main/java/pro3.java
@@ -0,0 +1,11 @@
+public class pro3
+{
+    public static void main(String[] args)
+    {
+        String a="raja";
+        String b="rani";
+        System.out.println(a);
+        System.out.println(b);
+
+    }
+}
